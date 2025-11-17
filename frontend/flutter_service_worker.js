43ec551f47245b0d809df7467491754dacca3e21
Gricon <gricon1@protonmail.com>
2025-11-17 14:09:57 +0800

    Update web app with posts, comments, and privacy features

--- frontend/flutter_service_worker.js
+++ frontend/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/fonts/MaterialIcons-Regular.otf": "ba4598fc972318f9e5a467478057b3cc",
+"assets/fonts/MaterialIcons-Regular.otf": "01136340267a17d14505de0a24479c3d",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
@@ -33,8 +33,8 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "index.html": "9f7985708d9a657674c44f9f836f508d",
 "/": "9f7985708d9a657674c44f9f836f508d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "4f09774609850c143f0f695ae0eaf377",
-"main.dart.js": "0e75f7b7e411c091d56ffa5451a7fc43"};
+"flutter_bootstrap.js": "c49d5212c8e2deac930565f6630b6175",
+"main.dart.js": "815af7d39c042e23cc264836ab0ffb89"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
